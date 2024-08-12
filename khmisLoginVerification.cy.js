b4fd0baa1bfd8ef8dc59d7a2a207707196f58560
Mary Muiruri <maryawat@gmail.com>
2024-08-12 05:36:23 +0300

    update khmislogin

--- khmisLoginVerification.cy.js
+++ khmisLoginVerification.cy.js
@@ -1,108 +1,84 @@
+/// <reference types="cypress"/>
+
 const loginPageUrl = 'https://qa.kenyahmis.org/openmrs/spa/login';
+
 describe('Kenya EMR Login', () => {
-  it('navigate', () => {
-    
+  beforeEach(() => {
+  it('should verify successful login', () => {
+
+    // Navigate to the login page before each test 
     cy.visit(loginPageUrl);
 
     cy.get('#username').type("admin")
     cy.contains('Continue').click()
     cy.get('#password').type("Admin123")
     cy.contains('Log in').click()
-    //cy.contains('Log in{Enter}')
+    
 
     // Check for specific element on the landing page
     // This element shoud be present only if the login is successful
     cy.contains('Total Visits Today').should('be.visible'),{ timeout: 400000 }   
-  })
-})
+  });
 
-describe('Login Error Handling for invalid Password', () => {
+
   it('should show an error message for invalid password', () => {
     // Visit the login page
     cy.visit(loginPageUrl);
 
-    // Enter username
-    cy.get('#username').type('admin');
+    cy.get('#username').type('admin'); // Enter username
 
-    // Click the "Continue" button (ensure this triggers the password field)
-    cy.contains('Continue').click();
-
-    // Enter invalid password
-    cy.get('#password').type('Admin1234');
-
-    // Click the login button
-    cy.get('button[type="submit"]').click();
-
-    // Verify error message is displayed with a custom timeout
-    cy.contains('Invalid username or password', { timeout: 60000 }).should('be.visible');
-  });
-});
-
-describe('Login Error Handling for invalid Username', () => {
-  it('should show an error message for invalid Username', () => {
-    // Visit the login page
-    cy.visit(loginPageUrl);
-
-    // Enter username
-    cy.get('#username').type('admin1');
-
-    // Click the "Continue" button (ensure this triggers the password field)
-    cy.contains('Continue').click();
-
-    // Enter invalid password
-    cy.get('#password').type('Admin123');
-
-    // Click the login button
-    cy.get('button[type="submit"]').click();
+    cy.contains('Continue').click(); // Click the "Continue" button (ensure this triggers the password field)
+    cy.get('#password').type('Admin1234'); // Enter invalid password
+    cy.get('button[type="submit"]').click(); // Click the login button
 
     // Verify error message is displayed with a custom timeout
     cy.contains('Invalid username or password', { timeout: 60000 }).should('be.visible');
   });
 
-});
 
-describe('Login Error Handling for both invalid Username and password', () => {
+  it('should return an error message for invalid Username', () => {
+    
+    cy.visit(loginPageUrl); // Visit the login page
+
+    cy.get('#username').type('admin1'); // Enter username
+    cy.contains('Continue').click(); // Click the "Continue" button (ensure this triggers the password field)
+    cy.get('#password').type('Admin123'); // Enter valid password
+    cy.get('button[type="submit"]').click(); // Click the login button
+
+    // Verify error message is displayed with a custom timeout
+    cy.contains('Invalid username or password', { timeout: 60000 }).should('be.visible');
+  });
+
+
+
   it('should show an error message for both invalid Username and password', () => {
-    // Visit the login page
-    cy.visit(loginPageUrl);
+    
+    cy.visit(loginPageUrl); // Visit the login page
 
-    // Enter username
-    cy.get('#username').type('admin1');
-
-    // Click the "Continue" button (ensure this triggers the password field)
-    cy.contains('Continue').click();
-
-    // Enter invalid password
-    cy.get('#password').type('Admin1234');
-
-    // Click the login button
-    cy.get('button[type="submit"]').click();
+    cy.get('#username').type('admin1'); // Enter invalid username
+    cy.contains('Continue').click(); // Click the "Continue" button (ensure this triggers the password field)
+    cy.get('#password').type('Admin1234'); // Enter invalid password
+    cy.get('button[type="submit"]').click(); // Click the login button
 
     // Verify error message is displayed with a custom timeout
     cy.contains('Invalid username or password', { timeout: 60000 }).should('be.visible');
   });
 
-  describe('Login Error Handling for username with alphanumeric(special) characters', () => {
-    it('should show an error message for both invalid Username and password', () => {
-      // Visit the login page
-      cy.visit(loginPageUrl);
+
+    it('should display an error message if username contains special characters', () => {
+      
+      cy.visit(loginPageUrl); // Visit the login page
   
-      // Enter username
-      cy.get('#username').type('admin##');
-  
-      // Click the "Continue" button (ensure this triggers the password field)
-      cy.contains('Continue').click();
-  
-      // Enter invalid password
-      cy.get('#password').type('Admin123');
-  
-      // Click the login button
-      cy.get('button[type="submit"]').click();
+      cy.get('#username').type('admin##'); // Enter username with special characters
+      cy.contains('Continue').click(); // Click the "Continue" button (ensure this triggers the password field)
+      cy.get('#password').type('Admin123'); // Enter invalid password
+      cy.get('button[type="submit"]').click(); // Click the login button
   
       // Verify error message is displayed with a custom timeout
       cy.contains('Invalid username or password', { timeout: 60000 }).should('be.visible');
     });
 
-});
 
+
+});
 });
